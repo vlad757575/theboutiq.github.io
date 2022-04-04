e885af1fbe1c327f6c5db31886b64f83c3b36cc7
vlad757575 <vld.lavrentiev@gmail.com>
2022-04-04 08:56:12 +0200

    7th commit

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConnexionController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="app_connexion")
+     */
+    public function index(): Response
+    {
+        return $this->render('connexion/index.html.twig', [
+            'controller_name' => 'ConnexionController',
+        ]);
+    }
+}
