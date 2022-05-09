36a9cdc332ed585931681f54d5b21f1ad5d0ac5b
vlad757575 <vld.lavrentiev@gmail.com>
2022-05-09 14:09:23 +0200

    23th commit meta is_granted controllers

--- src/Controller/AdresseLivraisonController.php
+++ src/Controller/AdresseLivraisonController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Utilisateur;
 
 /**
  * @Route("/adresse/livraison")
@@ -79,7 +80,7 @@ class AdresseLivraisonController extends AbstractController
      */
     public function delete(Request $request, AdresseLivraison $adresseLivraison, AdresseLivraisonRepository $adresseLivraisonRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$adresseLivraison->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $adresseLivraison->getId(), $request->request->get('_token'))) {
             $adresseLivraisonRepository->remove($adresseLivraison);
         }
 
