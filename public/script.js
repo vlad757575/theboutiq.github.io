5c401db99467b8011b32bc1bc5cf34f27cbca915
vlad757575 <vld.lavrentiev@gmail.com>
2022-07-22 18:22:39 +0200

    55th commit product img and css files

--- public/script.js
+++ public/script.js
@@ -1,25 +1,3 @@
-        //  const open = document.getElementById("open");
-        //  const modal_container = document.getElementById("my-modal_container");
-        //  const close = document.getElementById("close_my-modal");
-        //  const openContact =document.getElementById("open_contact")
-        //  const openSign =document.getElementById("open_sign")
-     
-        //  open.addEventListener("click", () => {
-        //    modal_container.classList.add("show");
-        //  });
-     
-        //  close.addEventListener("click", () => {
-        //    modal_container.classList.remove("show");
-        //  });
-
-        //  openContact.addEventListener("click", () => {
-        //   modal_container.classList.add("show");
-        // });
-
-        // openSign.addEventListener("click", () => {
-        //   modal_container.classList.add("show");
-        // });
-
 
        var modal  = document.getElementByCl('#mon-modal');
        var modalButton = document.querySelector('#modal-open');
